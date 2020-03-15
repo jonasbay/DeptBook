79ac16ce0103b0d52998e566cd76f70423a63a11
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-03-15 16:10:01 +0100

    Jonas

--- TheDeptBook/TheDeptBook/ViewModels/AddDeptViewModel.cs
+++ TheDeptBook/TheDeptBook/ViewModels/AddDeptViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace TheDeptBook.ViewModels
@@ -28,6 +30,21 @@ namespace TheDeptBook.ViewModels
             set
             {
                 SetProperty(ref currentDeptor, value);
+            }
+        }
+
+        ICommand _addValueCommand;
+
+        public ICommand AddValueCommand
+        {
+            get
+            {
+                return _addValueCommand ?? (_addValueCommand = new DelegateCommand(() =>
+                           {
+                               CurrentDeptor.Date = DateTime.Now.ToString("dd/mm/yyyy");
+                               currentDeptor.AddDeptToPerson(CurrentDeptor.Date, 12);
+                           }
+                       ));
             }
         }
 
